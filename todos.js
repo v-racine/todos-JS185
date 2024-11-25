c59565aa3789f9587cfcff303af70aea7ee04170
Valerie Racine <vracine21@gmail.com>
2024-11-25 11:44:09 -0800

    add pg-persistence

--- todos.js
+++ todos.js
@@ -4,7 +4,7 @@ const flash = require("express-flash");
 const session = require("express-session");
 const { body, validationResult } = require("express-validator");
 const store = require("connect-loki");
-const SessionPersistence = require("./lib/session-persistence");
+const PgPersistence = require("./lib/pg-persistence");
 
 const app = express();
 const host = "localhost";
@@ -48,7 +48,7 @@ app.use(flash());
 
 //Create a new datastore
 app.use((req, res, next) => {
-  res.locals.store = new SessionPersistence(req.session);
+  res.locals.store = new PgPersistence(req.session);
   next();
 });
 
